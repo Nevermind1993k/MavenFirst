36fd1b900a6260a435890bcde3edee7efae2dac3
Roman <nevermind1993k@gmail.com>
2017-09-06 21:21:33 +0300

    Initial commit

---
+++ src/main/java/org/nevermind/Main.java
@@ -0,0 +1,14 @@
+package org.nevermind;
+
+import org.joda.time.DateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DateTime jodaTime = new DateTime();
+        System.out.println(jodaTime.toDate());
+
+
+    }
+}
